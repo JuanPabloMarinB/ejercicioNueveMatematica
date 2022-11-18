9219da040427894b2c4df943c9d873bed1cb91d9
JuanPabloMarinB <youtuzonhd@gmail.com>
2022-11-18 13:07:07 -0300

    "Subiendo todos los proyctos de Java"

--- src/Entidades/Matematica.java
+++ src/Entidades/Matematica.java
@@ -1,2 +1,54 @@
-package Entidades;public class Matematica {
+package Entidades;
+
+public class Matematica {
+
+    private double numUno;
+
+    private double numDos;
+
+    private double numMayor;
+
+    private double numMenor;
+
+    public Matematica() {
+    }
+
+    public Matematica(double numUno, double numDos, double numMayor, double numMenor) {
+        this.numUno = numUno;
+        this.numDos = numDos;
+        this.numMayor = numMayor;
+        this.numMenor = numMenor;
+    }
+
+    public double getNumUno() {
+        return numUno;
+    }
+
+    public void setNumUno(double numUno) {
+        this.numUno = numUno;
+    }
+
+    public double getNumDos() {
+        return numDos;
+    }
+
+    public void setNumDos(double numDos) {
+        this.numDos = numDos;
+    }
+
+    public double getNumMayor() {
+        return numMayor;
+    }
+
+    public void setNumMayor(double numMayor) {
+        this.numMayor = numMayor;
+    }
+
+    public double getNumMenor() {
+        return numMenor;
+    }
+
+    public void setNumMenor(double numMenor) {
+        this.numMenor = numMenor;
+    }
 }
